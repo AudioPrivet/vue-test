e939c63a3a38c548758b78aa1ae61fbb8de8a354
latz <latz.dmitry@gmail.com>
2023-06-26 15:54:51 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-6 my-5">
+        <form>
+          <InputPassword v-model="password"/>
+        </form>
+        <code>
+          {{ password }}
+        </code>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import InputPassword from './components/InputPassword.vue'
+
+export default {
+  name: 'App',
+  components: {
+    InputPassword
+  },
+  data () {
+    return {
+      password: 'test123'
+    }
+  }
+}
+</script>
---
+++ src/components/InputPassword.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="input-group mb-3">
+    <input class="form-control" placeholder="Password">
+    <div class="input-group-append">
+      <span class="input-group-text">
+        <i class="fa fa-eye-slash" aria-hidden="true"></i>
+      </span>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
